fa9adc2f388865d2c22e8e7b8c9eb54465a1201d
Patrick <po44243@gmail.com>
2025-02-14 16:49:31 -0300

    sprites incluídas na tela

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from src.game import *
-from src.character import Player
+from src.character import Player, PlayerRunning
+
 lista = pygame.sprite.Group()
+player_run = PlayerRunning()
 player = Player()
-lista.add(player)
+sp = [player, player_run]
+for s  in sp:
+    lista.add(s)
 
 jogo = Game(800, 600, title="Jogo Pygame")
 
-jogo.run(todas_as_sprites=lista)
+jogo.run(todas_as_sprites=lista, tela=jogo.screen)
--- src/game.py
+++ src/game.py
@@ -17,19 +17,18 @@ class Game:
         self.screen = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption(self.title)
 
-    def run(self,todas_as_sprites):
+    def run(self,todas_as_sprites, tela):
         # Loop principal do jogo
         while True:
-            self.handle_events()  # Trata os eventos
-            self.update()         # Atualiza o estado do jogo
-            self.render()        # Desenha tudo na tela
-            todas_as_sprites.draw(self.screen)
+            
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    self.quit() 
+            self.update()     
+            todas_as_sprites.draw(tela)
             todas_as_sprites.update()
-    def handle_events(self):
-        # Trata os eventos do Pygame
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                self.quit()
+            pygame.display.flip()
+            self.render()
 
     def update(self):
         # Atualiza o estado do jogo (neste exemplo, não há nada para atualizar)
@@ -38,9 +37,7 @@ class Game:
     def render(self):
         # Preenche a tela com a cor preta
         self.screen.fill(Cores.PRETO)
-
-        # Atualiza a tela
-        pygame.display.flip()
+        
 
     def quit(self):
         # Encerra o Pygame e sai do programa
