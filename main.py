841bf144aab81383955e6442e53f3926af5424fe
Patrick <po44243@gmail.com>
2025-02-20 19:23:22 -0300

    refactor: melhorei a estrura da organização das animações

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from src.game import *
-from src.sprites.base_sprites import Ladino, LadinoRunningJump
+from src.sprites.ladino_animations import Player, PlayerRunning
 
 lista = pygame.sprite.Group()
 
-ladino = Ladino()
-ladino_running_jump = LadinoRunningJump()
+ladino = Player()
+ladino_run = PlayerRunning()
 
 lista.add(ladino)
-lista.add(ladino_running_jump)
+lista.add(ladino_run)
 
 jogo = Game(800, 600, title="Jogo Pygame")
 jogo.run(todas_as_sprites=lista, tela=jogo.screen)
